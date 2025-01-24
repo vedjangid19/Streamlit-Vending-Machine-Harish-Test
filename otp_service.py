e84314d4e7aef68415dd95059cadaa57c924f926
Ved Jangid <66615436+vedjangid19@users.noreply.github.com>
2025-01-24 21:08:44 +0530

    Update otp_service.py

--- otp_service.py
+++ otp_service.py
@@ -1,6 +1,25 @@
 import random
 from twilio.rest import Client
-from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER
+import streamlit as st
+# from config import TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, TWILIO_PHONE_NUMBER
+
+# # Generate a 4-digit OTP
+# def generate_otp():
+#     return random.randint(1000, 9999)
+
+# # Send OTP via SMS using Twilio API
+# def send_otp_sms(mobile, msg_body):
+#     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+#     print(f"mobile number in : {mobile}")
+#     message = client.messages.create(
+#         # body=f"Your OTP for vending machine: {otp}",
+#         body=msg_body,
+#         from_=TWILIO_PHONE_NUMBER,
+#         to=mobile
+#     )
+#     return message.sid
+
+
 
 # Generate a 4-digit OTP
 def generate_otp():
@@ -8,12 +27,19 @@ def generate_otp():
 
 # Send OTP via SMS using Twilio API
 def send_otp_sms(mobile, msg_body):
+    # Fetch Twilio credentials from Streamlit secrets
+    TWILIO_ACCOUNT_SID = st.secrets["TWILIO_ACCOUNT_SID"]
+    TWILIO_AUTH_TOKEN = st.secrets["TWILIO_AUTH_TOKEN"]
+    TWILIO_PHONE_NUMBER = st.secrets["TWILIO_PHONE_NUMBER"]
+    
     client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
-    print(f"mobile number in : {mobile}")
+    print(f"Mobile number in: {mobile}")
     message = client.messages.create(
-        # body=f"Your OTP for vending machine: {otp}",
         body=msg_body,
         from_=TWILIO_PHONE_NUMBER,
         to=mobile
     )
     return message.sid
+
+
+
